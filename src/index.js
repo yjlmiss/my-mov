56632ead62a56c2c289975fd9738988042295d31
yjlmiss <yjl4022@gmail.com>
2022-03-25 17:24:25 +0800

    mac

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import FilterableProductTable from './plugs/FilterableProductTable';
 import reportWebVitals from './reportWebVitals';
 
+const PRODUCTS = [
+  { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+  { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+  { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+  { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+  { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+  { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
+    <FilterableProductTable products={PRODUCTS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +24,5 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
